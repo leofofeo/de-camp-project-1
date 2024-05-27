8469797a43219cda48694ea47ce0b2d1547445c7
Alberto concepcion <aconc@MacBook-Pro-6.local>
2024-05-27 07:36:33 -0500

    added database views to the job

--- src/data/jobs/drop_tables.sql
+++ src/data/jobs/drop_tables.sql
@@ -3,3 +3,7 @@ drop table if exists senators cascade;
 drop table if exists company_annual_financial_data cascade;
 drop table if exists company_historical_data cascade;
 drop table if exists company_profiles cascade;
+
+drop view if exists company_metrics_view cascade;
+drop view if exists senator_trades_view cascade;
+drop view if exists top_industries_view cascade;
--- src/data/views/top_Industry_view.sql
+++ src/data/views/top_Industry_view.sql
@@ -5,9 +5,6 @@ SELECT
     cp.sector, 
     cp.sector_key, 
     SUM(cp.market_cap)
-    --AVG(chd.price) AS avg_price, 
-   -- MIN(chd.price) AS min_price, 
-    --MAX(chd.price) AS max_price
 FROM 
     public.company_historical_data chd
 JOIN 
@@ -19,4 +16,4 @@ GROUP BY
     cp.industry_key, 
     cp.sector, 
     cp.sector_key 
-order by 5 desc     
+order by 5 desc ;   
