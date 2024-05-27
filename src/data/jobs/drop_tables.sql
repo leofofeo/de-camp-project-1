d8812f0a9a35b4820d65e8feab2c8b264bdf3c97
Alberto concepcion <aconc@MacBook-Pro-6.local>
2024-05-27 07:52:25 -0500

    modified order of drop statements views and tables

--- src/data/jobs/drop_tables.sql
+++ src/data/jobs/drop_tables.sql
@@ -1,9 +1,10 @@
+
+drop view if exists company_metrics_view;
+drop view if exists senator_trades_view;
+drop view if exists top_industries_view;
+
 drop table if exists senator_trades cascade;
 drop table if exists senators cascade;
 drop table if exists company_annual_financial_data cascade;
 drop table if exists company_historical_data cascade;
 drop table if exists company_profiles cascade;
-
-drop view if exists company_metrics_view cascade;
-drop view if exists senator_trades_view cascade;
-drop view if exists top_industries_view cascade;
